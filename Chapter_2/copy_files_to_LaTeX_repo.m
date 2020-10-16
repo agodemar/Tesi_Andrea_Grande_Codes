fe27bb50fa83c7f761f1955a7d1abff085d50b56
Andrea Grande <andreagrande98@gmail.com>
2020-10-16 15:55:25 +0200

    added new exercise

---
+++ Chapter_2/copy_files_to_LaTeX_repo.m
@@ -0,0 +1,17 @@
+latex_dir = "../../Tesi_Andrea_Grande/Chapter_2/";
+subdirs = [ ...
+    "geometric_characteristics_of_a_straight_wing/", ...
+   
+    ];
+
+% create folders (skipped if folders exist)
+for i=1:length(subdirs)
+    [status, msg] = mkdir(strcat(latex_dir, subdirs(i)));
+end
+
+for i=1:length(subdirs)
+    status = copyfile( ...
+        strcat(subdirs(i),"data.tex"), ...
+        strcat(latex_dir, subdirs(i), "data.tex") ...
+    );
+end
---
+++ Chapter_2/geometric_characteristics_of_a_straight_wing.m
@@ -0,0 +1,33 @@
+clear all;close all; clc;
+
+b = 26 * u.m;
+lambda_C4=0*u.deg;
+c=2.5 * u.m;
+taper_ratio=1;
+lambda_LE= 0*u.deg;
+wing_surface_mt_squared=(b/2)*c*(1+taper_ratio);
+aspect_ratio=b^2 / wing_surface_mt_squared;
+mean_chord=(2/3)*c*(1+taper_ratio+taper_ratio^2)/(1+taper_ratio);
+x_le_mean_chord= (b/6)*((1+2*taper_ratio)/(1+taper_ratio))*tan(lambda_LE);
+y_mean_chord=(b/6)*((1+2*taper_ratio)/(1+taper_ratio));
+%% Write data file
+[status, msg] = mkdir("./Geometric characteristics of a straight wing"); % create folder first
+fid = fopen('./Geometric characteristics of a straight wing/data.tex', 'w');
+if (fid == -1)
+    fprintf("Cannot open file.")
+else
+    fprintf(fid, "\\def\\mySpanWingMT{%f}\n", b);
+    fprintf(fid, "\\def\\myChordRootWingMT{%f}\n", c);
+    fprintf(fid, "\\def\\myChordTipWingMT{%f}\n", c);
+    fprintf(fid, "\\def\\myTaperRatioWing{%f}\n", taper_ratio);
+    fprintf(fid, "\\def\\myAreaWingMTsquared{%f}\n", wing_surface_mt_squared);
+    fprintf(fid, "\\def\\myMACWingMT{%f}\n", mean_chord);
+    fprintf(fid, "\\def\\myYMACWingMT{%f}\n", y_mean_chord);
+    fprintf(fid, "\\def\\myXLEMACWingMT{%f}\n", x_le_mean_chord);
+    fprintf(fid, "\\def\\myLambdaCQuarter{%f}\n", lambda_C4);
+    fprintf(fid, "\\def\\myLambdaLEDeg{%f}\n", lambda_LE);
+    fprintf(fid, "\\def\\myLambdaLERad{%f}\n", lambda_LE/u.rad);
+
+    % ...
+    fclose(fid);
+end
